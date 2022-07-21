b3f3b68398737c399c5b2f543135fa48e4997f71
YWtpcmE <86475463+ooAkira@users.noreply.github.com>
2022-07-21 22:54:13 +0700

    Initial commit - initial code ✅

---
+++ js/data.js
@@ -0,0 +1,14 @@
+let getTime = new Date()
+let day = getTime.getDay()
+let month = getTime.getMonth()
+let year = getTime.getFullYear()
+
+let dataUser = {
+    name: "Akira",
+    age: Number(year - 2006),
+    education: "Student",
+    level: "High School",
+    description: "",
+    hobbies: "Coding",
+}
+
